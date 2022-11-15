30e28ae09a2ac1d86bec0ecc0c8030f9870a7e37
khatore <vikaskhatore123@gmail.com>
2022-11-15 13:26:09 +0530

    Add * print

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+'''Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.'''
+
+print("Hello World")
+for i in range(1,6):
+    print(i*'* ')
